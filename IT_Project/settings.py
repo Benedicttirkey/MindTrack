98686fa61370fa6f2c4d79040888ef1e53593fef
Benedict Tirkey <72373032+Benedicttirkey@users.noreply.github.com>
2026-03-17 22:50:47 +0000

    Add Whitenoise for static file storage
    On the line 202

--- IT_Project/settings.py
+++ IT_Project/settings.py
@@ -199,6 +199,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'IT_Project.urls'
 
 TEMPLATES = [
